71f94d02c468d6bc4e391fe6ebec191b88e61980
John LaGreco <johnlagreco@gmail.com>
2015-12-28 18:54:26 -0500

    Changed index & signin files. Altered app.rb

--- app.rb
+++ app.rb
@@ -3,4 +3,37 @@ require "sinatra/activerecord"
 
 set :database, "sqlite3:myblogdb.sqlite3"
 
+enable :sessions
+
 require "./models"
+
+get '/' do
+	@user = current_user
+	if @user
+	@users = User.all
+	erb :index
+	else
+		redirect 'sign-in'
+	end 
+end
+
+get '/sign-in' do
+	erb :signin
+end
+
+post 'sign-in' do
+	@user = User.where(first_name: params[:first_name]).first
+
+	if @user && @user.password == params[:password]
+		session[:user_id] = @user.id
+	 	redirect '/'
+	 else
+	 	redirect 'sign-in'
+	 end 
+end
+
+def current_user
+	if session[:user_id]
+	@current_user = User.find(session[:user_id])
+	end
+end
